77c77b5f115eb2c53dcc3fd7dd00a48c0673847c
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2025-10-23 11:41:39 +0200

    Fixed decrement counter bug

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
+import '../styles.css'
 
 const Counter = () => {
   const [count, setCount] = useState(0) // creating useState hook 
 
 //   btn handlers
   const incrementBtn = () => setCount(count + 1) // adding 1 to our count when btn is clicked
-  const decrementBtn = () => setCount(count + 1) // subtracting 1 from our count when btn is clicked
+  const decrementBtn = () => setCount(count - 1) // subtracting 1 from our count when btn is clicked
 
 
   return (
